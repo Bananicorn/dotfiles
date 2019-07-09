f1045f53cfcc933bbcb396dee16f07c1d34a97eb
bananicorn <ratschorr@gmail.com>
2019-07-10 00:05:34 +0200

    added xinput commands to .xinitrc

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ cp .bash_profile ~/.bash_profile
 cp .fishrc ~/.fishrc
 cp .xinitrc ~/.xinitrc
 ./setup-vim.sh
-mkdir ~/.config
+mkdir -p ~/.config
+mkdir -p ~/.config/i3
 cp -r i3 ~/.config/i3/config
