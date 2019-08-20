fdfb970cf09badbc4cd34a8e6f2c1a4acbda5019
bananicorn <ratschorr@gmail.com>
2019-08-20 11:49:09 +0200

    added cloning of plugins to setup-vim.sh removed creation of folder for ctrl-p plugin, since I won't use it anymore

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 cp .vimrc ~/.vimrc
 mkdir ~/.vim
-mkdir ~/.vim/bundle #this is for Ctrl-P
 mkdir ~/.vim/plugin
 mkdir ~/.vim/undofiles
 mkdir ~/.vim/swapfiles
+mkdir ~/.vim/sessions
+
+#install all the plugins
+cd ~/.vim/plugin
+git clone "https://github.com/scrooloose/nerdcommenter"
+git clone "https://github.com/wesQ3/vim-windowswap"
+git clone "https://github.com/editorconfig/editorconfig-vim"
