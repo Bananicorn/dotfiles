0ae0a5c56aa1f5bb10d80bee2896f56b84bd7c44
bananicorn <ratschorr@gmail.com>
2019-11-26 20:01:41 +0100

    finally symlinked i3 config instead of copying it and cleaned it up a little

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ ln -s ~/dotfiles/.fishrc ~/.fishrc
 ln -s ~/dotfiles/.xinitrc ~/.xinitrc
 ./setup-vim.sh
 
-#change this to a symbolic link too once brain works again
 mkdir -p ~/.config
 mkdir -p ~/.config/i3
-cp -r ~/dotfiles/i3 ~/.config/i3/config
+ln -s ~/dotfiles/i3_config ~/.config/i3/config
