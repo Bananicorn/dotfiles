247ce15465970025d518e7d64065190494818a4d
bananicorn <ratschorr@gmail.com>
2020-03-19 08:20:11 +0100

    added gitignore and linked+configured it in the setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,12 @@ ln -s ~/dotfiles/.fishrc ~/.fishrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.profile ~/.profile
 ln -s ~/dotfiles/.xinitrc ~/.xinitrc
+ln -s ~/dotfiles/.gitignore ~/.gitignore
 ./setup-vim.sh
 
 mkdir -p ~/.config
 mkdir -p ~/.config/i3
 ln -s ~/dotfiles/i3_config ~/.config/i3/config
+
+#if git is installed
+git config --global core.excludesfile '~/.gitignore'
