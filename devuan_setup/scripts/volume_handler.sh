7e67015e36936be2a233ccab2f646571d52b003d
bananicorn <ratschorr@gmail.com>
2022-10-10 17:42:19 +0200

    of course I'd fuck up the order of the colors

--- devuan_setup/scripts/volume_handler.sh
+++ devuan_setup/scripts/volume_handler.sh
@@ -8,10 +8,10 @@ else
 	pulsemixer --toggle-mute
 fi
 
-COLORS="#FF222222 #FF33DD33 #FF33DD33"
 VOLUME=$(pulsemixer --get-volume | grep -oP "^\d+")
+COLORS="#FF222222 #FF33DD33 #FFFF0000"
 if [ $(pulsemixer --get-mute) = "0" ]; then
-	COLORS="#FF222222 #FF33DD33 #FFFF0000"
+	COLORS="#FF222222 #FF33DD33 #FF33DD33"
 fi
 
 echo "$VOLUME $COLORS" > $2
