22e6f5630f0e2a7aec2f0ee51b6c6f08a42067f6
bananicorn <ratschorr@gmail.com>
2023-07-27 00:03:53 +0200

    use sudo in system setup

--- devuan_setup/setup_system.sh
+++ devuan_setup/setup_system.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #the bare essentials
-apt install -y vim
+sudo apt install -y vim
 ~/dotfiles/setup-vim.sh
-apt install -y git
+sudo apt install -y git
 cp ./scripts/vimv /usr/bin
 
 #window manager
-apt install -y sway
-apt install -y swaybg
+sudo apt install -y sway
+sudo apt install -y swaybg
 mkdir -p .config/sway
 ln -s ~/dotfiles/devuan_setup/config/sway/config ~/.config/sway/config
 rm -rf ~/.profile
@@ -16,35 +16,35 @@ ln -s ~/dotfiles/devuan_setup/.profile ~/.profile
 ln -s ~/dotfiles/devuan_setup/config/user-dirs.dirs ~/.config/
 
 #install terminal emulator
-apt install -y foot
+sudo apt install -y foot
 ln -s ~/dotfiles/devuan_setup/config/foot ~/.config/foot
 
 #dialog boxes
-apt install -y zenity
+sudo apt install -y zenity
 
 #image viewer
-apt install -y imv
+sudo apt install -y imv
 
 #let there be sound (over bluetooth too)!
-apt install -y pulseaudio
-apt install -y pulseaudio-module-bluetooth
-apt install -y pulsemixer
-apt install -y bluez
-apt install -y bluez-firmware
-apt install -y bluez-tools
+sudo apt install -y pulseaudio
+sudo apt install -y pulseaudio-module-bluetooth
+sudo apt install -y pulsemixer
+sudo apt install -y bluez
+sudo apt install -y bluez-firmware
+sudo apt install -y bluez-tools
 
 #screen brightness control
-apt install -y brightnessctl
+sudo apt install -y brightnessctl
 
 #battery measurement
-apt install -y acpi
-apt install -y acpid
+sudo apt install -y acpi
+sudo apt install -y acpid
 
 #displays "progress" bars - used for brightness and volume
-apt install -y wob
+sudo apt install -y wob
 
 #launcher
-apt install -y wofi
+sudo apt install -y wofi
 mkdir -p .config/wofi
 ln -s ~/dotfiles/devuan_setup/config/wofi/style.css ~/.config/wofi/style.css
 ln -s ~/dotfiles/devuan_setup/config/wofi/config ~/.config/wofi/config
@@ -52,28 +52,28 @@ ln -s ~/dotfiles/devuan_setup/config/wofi/config ~/.config/wofi/config
 mkdir -p ~/.local/share/applications
 
 #wifi and bluetooth
-apt install -y connman-gtk
+sudo apt install -y connman-gtk
 
 #automount external drives
-apt install -y udiskie
+sudo apt install -y udiskie
 
 #love2d stuff
-apt install -y love
-apt install -y apktool
+sudo apt install -y love
+sudo apt install -y apktool
 
 #actual programs
-apt install -y zsh
-apt install -y firefox-esr
-apt install -y inkscape
-apt install -y grimshot
-apt install -y thunar
-apt remove -y thunar-volman
+sudo apt install -y zsh
+sudo apt install -y firefox-esr
+sudo apt install -y inkscape
+sudo apt install -y grimshot
+sudo apt install -y thunar
+sudo apt remove -y thunar-volman
 
 #work stuff
-apt install -y git-flow
-apt install -y docker.io
-apt install -y docker-compose
-apt install -y npm
+sudo apt install -y git-flow
+sudo apt install -y docker.io
+sudo apt install -y docker-compose
+sudo apt install -y npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 DOCKER_COMPOSE_VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
 DOCKER_COMPOSE_PATH=/usr/local/bin/docker-compose
